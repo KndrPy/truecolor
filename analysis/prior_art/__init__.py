bf71b20c486db600811787e308d9f1704661a7a8
OceanBz <bruno@alumni.upenn.edu>
2026-07-14 21:30:39 -0700

    Activate hardened version relationship inference

--- analysis/prior_art/__init__.py
+++ analysis/prior_art/__init__.py
@@ -5,9 +5,11 @@ from analysis.prior_art import mutable_corpus_service as _mutable_corpus_service
 from analysis.prior_art.mutable_corpus_enterprise_overrides import (
     build_document_versions as _build_document_versions,
     infer_primary_identity as _infer_primary_identity,
+    infer_version_relationships as _infer_version_relationships,
 )
 
 _mutable_corpus_enterprise.build_document_versions = _build_document_versions
+_mutable_corpus_enterprise.infer_version_relationships = _infer_version_relationships
 _mutable_corpus_service.infer_identity = _infer_primary_identity
 
 __all__ = []
